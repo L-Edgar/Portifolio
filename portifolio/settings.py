77296884516ff43a63d6edd2bdcf99340056aa97
L-Edgar <lelishaedgar10@gmail.com>
2024-01-23 18:57:57 +0300

    added styles

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleWare",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,8 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
-STATIC_ROOT=os.path.join(BASE_DIR,"assets")
+#STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
+STATIC_ROOT=BASE_DIR/"assets"
+
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
