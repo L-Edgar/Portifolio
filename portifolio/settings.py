ee4b4b9a6b82b0cc5092680c86f3dd9287d00439
L-Edgar <lelishaedgar10@gmail.com>
2024-08-07 11:43:39 +0300

    new changes

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-rx8!&0xn4es32*yh)))!vgb80mja-n3ls26+jqz$p*3#aws!^6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -129,7 +129,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 
-STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
