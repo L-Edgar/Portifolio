605c3ed188509bfc2f2efe6ecec66c7a19e70929
L-Edgar <lelishaedgar10@gmail.com>
2024-08-07 13:11:20 +0300

    tired

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 
 # Install pip packages
-python3.10 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python3.10 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
