2b8b6f266bc49385312193917399f321987c1a07
L-Edgar <lelishaedgar10@gmail.com>
2024-08-10 10:22:31 +0300

    added aws

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -139,3 +139,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+AWS_ACCESS_KEY_ID = 'AKIA6GBMFUW72YGB2LGB'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'edgarfolder'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
