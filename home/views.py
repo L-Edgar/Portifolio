0ff3343f69e4453cfcaa133c525a78aa75d3dedd
LIISO EDGAR <98767098+L-Edgar@users.noreply.github.com>
2025-10-25 10:42:51 +0300

    added email

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Project, Testimonial, Skill
 from .forms import FeedbackForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 # Create your views here.
@@ -13,4 +15,23 @@ def home(request):
 
 def feedback_view(request):
     form=FeedbackForm()
+    if request.method=='POST':
+        form=FeedbackForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+            message=form.cleaned_data['message']
+            
+             # Email content
+            subject = f"New Feedback from {name}"
+            full_message=f"Message from {name} ({email}):\n\n{message}"
+            send_mail(
+                subject,
+                full_message,
+                settings.DEFAULT_FROM_EMAIL,
+                ['lelishaedgar10@gmail.com'],  # <-- Replace with your own email
+                fail_silently=False,
+            )
+            
+            #return render(request, 'index.html', {'form': FeedbackForm(), 'success': True})
     return render(request, 'index.html', {'form': form})
--- portifolio/settings.py
+++ portifolio/settings.py
@@ -29,7 +29,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+#EMAIL_USE_TLS = True
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'  # Literally the word 'apikey'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = config('EMAIL_ADDRESS') 
 
 
 
@@ -85,11 +98,14 @@ WSGI_APPLICATION = "portifolio.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "railway",
-        "USER":"postgres",
-        "PASSWORD":"hHvwRsThexraLjITpsSznDMzpjkOechU",
-        "HOST":"viaduct.proxy.rlwy.net",
-        "PORT":"58070",
+        "NAME":"defaultdb",
+        "USER":"avnadmin",
+        "PASSWORD":"AVNS_WzxyOTLZCF-BgFQJfsZ",
+        "HOST":"edgar-db-edgar-portifolio.j.aivencloud.com",
+        "PORT":15275,
+        'OPTIONS': {
+                'sslmode': 'require',       # Aiven typically requires SSL
+            },
         
         
     }
